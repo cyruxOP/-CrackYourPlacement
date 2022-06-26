bf1f45c19d669ccec34d9d60f17703c7d480b773
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-06-27 02:10:24 +0530

    Time: 965 ms (42.41%), Space: 237.4 MB (7.71%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,44 +1,48 @@
 class Solution {
 public:
-    int merge(vector<int> &nums,int lo,int mid,int hi)
+    int mer(vector<int> &nums,int lo,int mid,int hi)
     {
         int cnt=0;
-        int j= mid+1;
+        int j=mid+1;
+        
         for(int i=lo;i<=mid;i++)
         {
             while(j<=hi && nums[i]>2LL*nums[j])
+            {
                 j++;
-            cnt+= (j-(mid+1));
+            }
+            cnt+=(j-(mid+1));
         }
         vector<int> temp;
-        int l=lo,r=mid+1;
-        while(l<=mid and r<=hi)
+      int l=lo,r=mid+1;
+        while(l<=mid && r<=hi)
         {
             if(nums[l]<nums[r])
                 temp.push_back(nums[l++]);
             else
-                temp.push_back(nums[r++]);
+                temp.push_back(nums[r++]);     
         }
         while(l<=mid)
             temp.push_back(nums[l++]);
-        while(r<=hi)
+         while(r<=hi)
             temp.push_back(nums[r++]);
         for(int i=lo;i<=hi;i++)
             nums[i]=temp[i-lo];
-        return cnt;  
+        
+        return cnt;
     }
     
     
     
-    int ms(vector<int> &nums,int lo,int hi)
+    int ms(vector<int>& nums,int lo,int hi)
     {
         if(lo>=hi) return 0;
         int mid = lo + (hi-lo)/2;
         int inv = ms(nums,lo,mid);
-        inv += ms(nums,mid+1,hi);
-        inv += merge(nums,lo,mid,hi);
+        inv+=ms(nums,mid+1,hi);
+        inv+=mer(nums,lo,mid,hi);
         return inv;
-    }
+     }
     int reversePairs(vector<int>& nums) {
         return ms(nums,0,nums.size()-1);
     }
