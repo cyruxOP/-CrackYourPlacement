a4342406794eed506c6f83f5bc4b8abacfdf665b
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-07-03 18:01:56 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (89.57%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
    void sum(int ind, vector<vector<int>> &ans, vector<int> &nums,vector<int> &temp,int n)
     {
-        if(ind==n)
-        {
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[ind]);
-        sum(ind+1,ans,nums,temp,n);
-        temp.pop_back();
-       
-        while(ind+1<n && nums[ind]==nums[ind+1])
-            ind++;
-       sum(ind+1,ans,nums,temp,n);
+      ans.push_back(temp);
+       for(int i=ind;i<n;i++)
+       {
+           if(i!=ind && nums[i]==nums[i-1]) continue;
+           temp.push_back(nums[i]);
+           sum(i+1,ans,nums,temp,n);
+           temp.pop_back();
+       }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
