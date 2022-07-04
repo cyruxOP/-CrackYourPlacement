7ca90e67f9a6d458173fe09624e3a276825fe2d9
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-07-04 18:46:21 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 class Solution{
     public:
-    void solve(int i,int j,int n,vector<vector<int>> &m,string tmp,vector<string> &ans,int di[],int dj[],vector<vector<int>> &vis)
+    void solve(int i,int j,int n,vector<vector<int>> &m,string tmp,vector<string> &ans,int di[],int dj[])
     {
         if(i==n-1 && j==n-1)
         {
@@ -22,21 +22,20 @@ class Solution{
         {
             int ni = i+di[t];
             int nj = j+dj[t];
-            if(ni<n && nj<n && ni>=0 && nj>=0 && m[ni][nj]==1 && !vis[ni][nj])
+            if(ni<n && nj<n && ni>=0 && nj>=0 && m[ni][nj]==1)
             {
-                vis[i][j]=1;
-                solve(ni,nj,n,m,tmp+d[t],ans,di,dj,vis);
-                vis[i][j]=0;
+                m[i][j]=0;
+                solve(ni,nj,n,m,tmp+d[t],ans,di,dj);
+                m[i][j]=1;
             }
         }
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
         vector<string> ans;
-        vector<vector<int>> vis(n,vector<int>(n,0));
         int di[] ={1,0,0,-1};
         int dj[] ={0,-1,1,0};
-        if(m[0][0]==1)solve(0,0,n,m,"",ans,di,dj,vis);
+        if(m[0][0]==1)solve(0,0,n,m,"",ans,di,dj);
         return ans;
         
     }
