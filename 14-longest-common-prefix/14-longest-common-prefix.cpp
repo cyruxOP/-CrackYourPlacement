7f8a5a6a942007c66d92a4a346100159956c5903
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-07-12 21:46:37 +0530

    Time: 18 ms (7.13%), Space: 15.5 MB (6.35%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res="";
+        for(int i=0;i<strs[0].size();i++)
+        {
+            for(string s: strs)
+            {
+                if(i==s.size()|| s[i] != strs[0][i])
+                    return s.substr(0,i);
+                
+            }
+            res+=strs[0][i];
+        }
+        return res;
+    }
+};
