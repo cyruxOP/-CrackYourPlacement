d72562e74b38e1338e84abb46b404caadc6e4901
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-07-13 19:06:08 +0530

    Time: 14 ms (38.55%), Space: 13.8 MB (26.54%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int pro=1,mx=INT_MIN;
+        int pro=1,pro2=1,mx=INT_MIN,n=nums.size();
         for(int i=0;i<nums.size();i++)
         {
             pro*=nums[i];
+            pro2*=nums[n-i-1];
             mx = max(mx,pro);
+            mx=max(mx,pro2);
             if(nums[i]==0)
             {
                 pro=1;
             }
+            if(nums[n-i-1]==0)
+                pro2=1;
         }
-        pro=1;
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-            pro*=nums[i];
-            mx=max(mx,pro);
-            if(nums[i]==0)
-                pro=1;
-        }
-        return mx;
+     return mx;
     }
 };
