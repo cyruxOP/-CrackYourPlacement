b2accd216ec8e830c8c4add20071d44019a1dfdf
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-07-14 22:48:28 +0530

    Time: 6 ms (83.65%), Space: 9 MB (22.53%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    int f(int i,int j,int n,vector<vector<int>>&tr,vector<vector<int>>&dp)
+    int f(int m,int n,vector<vector<int>>&tr,vector<vector<int>>&dp)
     {
-        if(i==n-1) return tr[n-1][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int dow = tr[i][j]+ f(i+1,j,n,tr,dp);
-        int dg =tr[i][j] + f(i+1,j+1,n,tr,dp);
-        
-        return dp[i][j]= min(dow,dg);
+       for(int j=0;j<n;j++)
+       {
+           dp[m-1][j]=tr[m-1][j];
+       }
+        for(int i=m-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int lf = tr[i][j] + dp[i+1][j];
+                int dg = tr[i][j] + dp[i+1][j+1];
+                
+                dp[i][j]=min (lf,dg);
+            }
+        }
+        return dp[0][0];
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         int n = triangle[m-1].size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return f(0,0,m,triangle,dp);
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        return f(m,n,triangle,dp);
     }
 };
