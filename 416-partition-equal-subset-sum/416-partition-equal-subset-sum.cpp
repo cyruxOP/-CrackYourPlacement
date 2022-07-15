566083be7195815827f0cce03c59d054461496d8
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-07-15 20:31:22 +0530

    Time: 541 ms (18.78%), Space: 75.1 MB (31.50%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int ts=0;
+        for(int i=0;i<nums.size();i++)
+            ts+=nums[i];
+        if(ts&1) return 0;
+        ts/=2;
+        int tar=ts;
+        vector<vector<int>> dp(n,vector<int>(ts+1,0));
+        for(int i=0;i<nums.size();i++)
+            dp[i][0]=1;
+        if(nums[0]<=tar) dp[0][nums[0]]=1;
+        for(int i=1;i<n;i++)
+            for(int t=1;t<=tar;t++)
+            {
+                int nt = dp[i-1][t];
+                int tk = 0;
+                if(nums[i]<=t)
+                    tk=
+                dp[i-1][t-nums[i]];
+                
+                dp[i][t]= nt | tk;
+                
+            }
+        
+        return dp[n-1][tar];
+            
+    }
+};
