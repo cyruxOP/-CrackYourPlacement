0c7791b29750092dde88859bfcc54a310bdd9474
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-07-19 01:04:25 +0530

    Time: 14 ms (82.65%), Space: 10.4 MB (67.38%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -14,7 +14,22 @@ public:
     
     int lengthOfLIS(vector<int>& nums) {
         int n =nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return f(0,-1,n,nums,dp);
+        vector<int> temp;
+        temp.push_back(nums[0]);int l=1;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>temp.back())
+            {
+                temp.push_back(nums[i]);
+                l++;
+            }
+            else
+            {
+                int ind = lower_bound(temp.begin(),temp.end(),nums[i]) -temp.begin();
+                temp[ind]=nums[i];
+            }
+        }
+        return l;
+        
     }
 };
