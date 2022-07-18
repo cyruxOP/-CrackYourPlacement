92cb1afaffb4def7ffb08f3d3837dd684217fd4b
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-07-19 00:35:22 +0530

    Time: 2651 ms (5.00%), Space: 291.8 MB (10.43%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int f(int i,int pi,int n,vector<int>&nums,vector<vector<int>>&dp)
+    {
+        if(i==n) return 0;
+        if(dp[i][pi+1]!=-1) return dp[i][pi+1];
+        int len = f(i+1,pi,n,nums,dp);
+        
+        if(pi==-1 || nums[i]>nums[pi])
+        len = max(len,1+f(i+1,i,n,nums,dp));
+        return dp[i][pi+1]=len;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n =nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return f(0,-1,n,nums,dp);
+    }
+};
