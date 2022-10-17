576cf94fc12f7677b23c8382c5548cc6c83f719e
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-10-17 14:20:47 +0530

    Time: 1281 ms (28.57%), Space: 139.6 MB (28.57%) - LeetHub

---
+++ 2440-create-components-with-same-value/2440-create-components-with-same-value.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int>arr;
+    vector<vector<int>> g;
+    int dfs(int ind,int par,const int tar)
+    {
+        int val = arr[ind];
+        for(auto it:g[ind])
+        {
+            if(it==par)continue;
+            val+=dfs(it,ind,tar);
+        }
+        if(val==tar) return 0;
+        return val;
+    }
+    
+    
+    int componentValue(vector<int>& nums, vector<vector<int>>& edges) {
+        g.clear();arr.clear();
+        int n = nums.size();
+        g.resize(n);
+        arr.resize(n);
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            arr[i]=nums[i];
+        }
+        for(auto it:edges)
+        {
+            g[it[0]].push_back(it[1]);
+            g[it[1]].push_back(it[0]);
+        }
+        int result=0;
+        for(int p=1;p<=sum;p++)
+        {
+            if(sum%p!=0)continue;
+            if(dfs(0,-1,sum/p)!=0) continue;
+            result=max(result,p-1);
+        }
+        return result;
+    }
+};
