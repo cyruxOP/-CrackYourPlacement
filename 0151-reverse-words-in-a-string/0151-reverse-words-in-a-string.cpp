7e20912a35fab219d8add8d4683eb9ba0729a601
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2022-11-13 07:58:32 +0530

    Time: 6 ms (84.31%), Space: 6.9 MB (99.10%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+           int n = s.size(),i=0,j=0,lind=0;
+        reverse(s.begin(),s.end());
+        while(j<n)
+        {
+            while(j<n && s[j]==' ')
+                j++;
+            int sind=i;
+            while(j<n && s[j]!=' ')
+            {
+                s[i++]=s[j++];
+                    lind=i;
+            }
+            reverse(s.begin()+sind,s.begin()+lind);
+            if(i<n)
+            s[i++]=' ';
+        }
+        s.resize(lind);
+        return s;
+    }
+};
