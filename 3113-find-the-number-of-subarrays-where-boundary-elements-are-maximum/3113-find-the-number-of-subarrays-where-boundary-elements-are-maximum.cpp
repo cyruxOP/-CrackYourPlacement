ae39cafbd3d95a23268b9b9e6b526a300356888e
cyruxOP <104842529+cyruxOP@users.noreply.github.com>
2024-04-17 22:46:51 +0530

    Time: 674 ms (8.83%), Space: 175.8 MB (21.36%) - LeetHub

---
+++ 3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum/3113-find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    using ll = long long;
+    long long numberOfSubarrays(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        stack<int> st;
+        vector<int> nge(n,n);
+        
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<= nums[i]){
+                st.pop();
+            }
+            
+            if(st.empty()){
+                nge[i]=n;
+            }else{
+                nge[i]=st.top();
+            }
+            
+            st.push(i);
+        }
+        
+        map<int,vector<int>> mp;
+        
+        for(int i=0;i<n;i++){
+            mp[nums[i]].push_back(nge[i]);
+        }
+        ll ans = 0;
+        
+        for(auto it:mp){
+            int l =-1;
+            ll cnt =0;
+            cout<<it.first<<" --> ";
+            for(auto i:it.second){
+                cout<<i<<" ";
+                if(l==-1){
+                    l=i;
+                    cnt++;
+                    continue;
+                }
+                
+                if(i!=l){
+                    ans+= (cnt* (cnt+1))/2;
+                    cnt=1;
+                }
+                else{
+                    cnt++;
+                }
+                l=i;
+            }
+              ans += (cnt * (cnt+1))/2;
+            cout<<'\n';
+            
+          
+            
+        }
+        
+        return ans;
+    }
+};
